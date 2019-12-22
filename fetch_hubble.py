e5f6a6d9a4cce5ee223e1ea9504317f98fbd1700
Faezragore <e.v.tarasevich@mail.ru>
2019-12-23 01:13:42 +0300

    first download

---
+++ fetch_hubble.py
@@ -0,0 +1,52 @@
+import os
+import pathlib
+import argparse
+import requests
+
+def output_links_to_the_first_image_in_the_Hubble_API(url):
+    list_of_links = []
+    response = requests.get(url, verify=False)
+    number_index = response.text
+    number_index = number_index[number_index.find('image_files') + 14:-2].split('"')
+    for i in number_index:
+        if '//' in i:
+            list_of_links.append('http:'+i)
+    return list_of_links      
+
+def to_show_the_extension_of_the_image(url):
+    return url.split('.')[-1]
+
+def download_pictures_from_Hubble(id_image):
+    path = os.getcwd()
+    pathlib.Path('%s/images' % (path)).mkdir(parents=True, exist_ok=True)
+
+    list_of_links = output_links_to_the_first_image_in_the_Hubble_API('http://hubblesite.org/api/v3/image/%s' % (id_image))
+    url = list_of_links[-1]
+    response = requests.get(url, verify=False)
+    print(to_show_the_extension_of_the_image(list_of_links[-1]))
+    
+    with open('%s/images/%s.%s' % (path,id_image,to_show_the_extension_of_the_image(list_of_links[-1])), 'wb') as file:
+        file.write(response.content)
+
+def download_the_collection_of_pictures_API_hubble(the_name_of_the_collection):
+    url = 'http://hubblesite.org/api/v3/images/%s?page=all' % (the_name_of_the_collection)
+    response = requests.get(url, verify=False)
+    list_id_images = response.text.split(',')
+
+    for id_images in list_id_images:
+        if '{"id":' in id_images:
+            print(id_images.split(':')[1] ,sep=' ')
+            download_pictures_from_Hubble(id_images.split(':')[1])
+
+
+def main():
+    parser = argparse.ArgumentParser(description='enter a collection name')
+    parser.add_argument('the_name_of_the_collection', help='collection name')
+    args = parser.parse_args()
+    the_name_of_the_collection = args.the_name_of_the_collection
+    
+    download_the_collection_of_pictures_API_hubble(the_name_of_the_collection)
+
+
+if __name__ == '__main__':
+    main()
---
+++ upload_photos_to_instagram.py
@@ -0,0 +1,54 @@
+import os
+from os import listdir
+from os.path import isfile
+from os.path import join as joinpath
+import time
+import argparse
+
+from dotenv import load_dotenv
+load_dotenv()
+from PIL import Image
+from instabot import Bot
+
+def list_of_files_in_a_folder(the_path_to_the_folder):   
+    mypath = "%s" % (the_path_to_the_folder)
+    for photo_name_and_extension in listdir(mypath):
+        if isfile(joinpath(mypath,photo_name_and_extension)):
+            find_out_the_size_of_the_picture(the_path_to_the_folder,photo_name_and_extension)
+         
+def find_out_the_size_of_the_picture(the_path_to_the_folder,photo_name_and_extension):
+    image = Image.open("%s/%s" % (the_path_to_the_folder,photo_name_and_extension))
+    if max(image.size) == image.size[0]:
+        picture_cropping_length =(int((max(image.size) - min(image.size))/2))
+        coordinates = (picture_cropping_length, 0, image.width - picture_cropping_length, image.height)
+        cropped = image.crop(coordinates)
+        cropped.save("%s/pize/%s" % (the_path_to_the_folder,photo_name_and_extension))
+    elif max(image.size) == image.size[1]:
+        picture_cropping_length =(int((max(image.size) - min(image.size))/2))
+        coordinates = (0, picture_cropping_length, image.width, image.height - picture_cropping_length)
+        cropped = image.crop(coordinates)
+        cropped.save("%s/pize/%s" % (the_path_to_the_folder,photo_name_and_extension))
+    else:
+        print("circumcision is not required")
+
+def instabot(your_login,your_password,the_path_to_the_folder):
+    bot = Bot()
+    bot.login(username=your_login, password=your_password)
+    for photo_name_and_extension in listdir(the_path_to_the_folder):
+        timeout = 60
+        time.sleep(timeout)
+        if isfile(joinpath(the_path_to_the_folder,photo_name_and_extension)):
+		    #find_out_the_size_of_the_picture(the_path_to_the_folder,photo_name_and_extension)
+            bot.upload_photo("%s/%s" % (the_path_to_the_folder,photo_name_and_extension), caption="Super Typhoon Maysak ")
+
+def main():
+    your_login = os.getenv("LOGIN")
+    your_password = os.getenv("LOGIN_PASSWORD")
+    parser = argparse.ArgumentParser(description='Specify the path to the folder where the photos will be downloaded')
+    parser.add_argument('path', help='your way to the folder')
+    args = parser.parse_args()
+    the_path_to_the_folder = args.path
+    instabot(your_login,your_password,the_path_to_the_folder)
+
+if __name__ == '__main__':
+    main()
