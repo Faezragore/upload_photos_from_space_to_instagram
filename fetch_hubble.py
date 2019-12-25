73de570e988038cf70bcb6b0f9592b51518b9647
Faezragore <30468128+Faezragore@users.noreply.github.com>
2019-12-25 22:17:28 +0300

    Update fetch_hubble.py

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -11,7 +11,7 @@ def output_links_to_the_first_image_in_the_Hubble_API(url):
     for i in number_index:
         if '//' in i:
             list_of_links.append('http:'+i)
-    return list_of_links      
+    return list_of_links
 
 def to_show_the_extension_of_the_image(url):
     return url.split('.')[-1]
@@ -23,7 +23,7 @@ def download_pictures_from_Hubble(id_image):
     list_of_links = output_links_to_the_first_image_in_the_Hubble_API('http://hubblesite.org/api/v3/image/%s' % (id_image))
     url = list_of_links[-1]
     response = requests.get(url, verify=False)
-    
+
     with open('%s/images/%s.%s' % (path,id_image,to_show_the_extension_of_the_image(list_of_links[-1])), 'wb') as file:
         file.write(response.content)
 
@@ -35,13 +35,13 @@ def download_the_collection_of_pictures_API_hubble(the_name_of_the_collection):
     for id_images in list_id_images:
         if '{"id":' in id_images:
             download_pictures_from_Hubble(id_images.split(':')[1])
-            
+
 def main():
     parser = argparse.ArgumentParser(description='enter a collection name')
     parser.add_argument('the_name_of_the_collection', help='collection name')
     args = parser.parse_args()
     the_name_of_the_collection = args.the_name_of_the_collection
-    
+
     download_the_collection_of_pictures_API_hubble(the_name_of_the_collection)
 
 
