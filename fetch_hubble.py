15668a90a963c2e4b79885b4f7ffdba68379cd4a
Faezragore <30468128+Faezragore@users.noreply.github.com>
2019-12-25 22:20:33 +0300

    Update fetch_hubble.py

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -3,6 +3,7 @@ import pathlib
 import argparse
 import requests
 
+
 def output_links_to_the_first_image_in_the_Hubble_API(url):
     list_of_links = []
     response = requests.get(url, verify=False)
@@ -13,8 +14,10 @@ def output_links_to_the_first_image_in_the_Hubble_API(url):
             list_of_links.append('http:'+i)
     return list_of_links
 
+
 def to_show_the_extension_of_the_image(url):
     return url.split('.')[-1]
+
 
 def download_pictures_from_Hubble(id_image):
     path = os.getcwd()
@@ -27,6 +30,7 @@ def download_pictures_from_Hubble(id_image):
     with open('%s/images/%s.%s' % (path,id_image,to_show_the_extension_of_the_image(list_of_links[-1])), 'wb') as file:
         file.write(response.content)
 
+
 def download_the_collection_of_pictures_API_hubble(the_name_of_the_collection):
     url = 'http://hubblesite.org/api/v3/images/%s?page=all' % (the_name_of_the_collection)
     response = requests.get(url, verify=False)
@@ -35,6 +39,7 @@ def download_the_collection_of_pictures_API_hubble(the_name_of_the_collection):
     for id_images in list_id_images:
         if '{"id":' in id_images:
             download_pictures_from_Hubble(id_images.split(':')[1])
+
 
 def main():
     parser = argparse.ArgumentParser(description='enter a collection name')
