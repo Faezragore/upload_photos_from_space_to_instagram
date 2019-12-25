dfcebb958ee0d6b001d428cc287f34b7c414c057
Faezragore <30468128+Faezragore@users.noreply.github.com>
2019-12-26 01:11:26 +0300

    Update upload_photos_to_instagram.py

--- upload_photos_to_instagram.py
+++ upload_photos_to_instagram.py
@@ -4,41 +4,28 @@ from os.path import isfile
 from os.path import join as joinpath
 import time
 import argparse
-
 from dotenv import load_dotenv
-load_dotenv()
 from PIL import Image
 from instabot import Bot
+load_dotenv()
 
-def list_of_files_in_a_folder(the_path_to_the_folder):   
+
+def list_of_files_in_a_folder(the_path_to_the_folder):
     mypath = "%s" % (the_path_to_the_folder)
     for photo_name_and_extension in listdir(mypath):
-        if isfile(joinpath(mypath,photo_name_and_extension)):
-            find_out_the_size_of_the_picture(the_path_to_the_folder,photo_name_and_extension)
-         
-def find_out_the_size_of_the_picture(the_path_to_the_folder,photo_name_and_extension):
-    image = Image.open("%s/%s" % (the_path_to_the_folder,photo_name_and_extension))
-    if max(image.size) == image.size[0]:
-        picture_cropping_length =(int((max(image.size) - min(image.size))/2))
-        coordinates = (picture_cropping_length, 0, image.width - picture_cropping_length, image.height)
-        cropped = image.crop(coordinates)
-        cropped.save("%s/pize/%s" % (the_path_to_the_folder,photo_name_and_extension))
-    elif max(image.size) == image.size[1]:
-        picture_cropping_length =(int((max(image.size) - min(image.size))/2))
-        coordinates = (0, picture_cropping_length, image.width, image.height - picture_cropping_length)
-        cropped = image.crop(coordinates)
-        cropped.save("%s/pize/%s" % (the_path_to_the_folder,photo_name_and_extension))
-    else:
-        print("circumcision is not required")
+        if isfile(joinpath(mypath, photo_name_and_extension)):
+            find_out_the_size_of_the_picture(the_path_to_the_folder, photo_name_and_extension)
 
-def instabot(your_login,your_password,the_path_to_the_folder):
+
+def instabot(your_login, your_password, the_path_to_the_folder):
     bot = Bot()
     bot.login(username=your_login, password=your_password)
     for photo_name_and_extension in listdir(the_path_to_the_folder):
         timeout = 60
         time.sleep(timeout)
-        if isfile(joinpath(the_path_to_the_folder,photo_name_and_extension)):
-            bot.upload_photo("%s/%s" % (the_path_to_the_folder,photo_name_and_extension), caption="Super Typhoon Maysak ")
+        if isfile(joinpath(the_path_to_the_folder, photo_name_and_extension)):
+            bot.upload_photo("%s/%s" % (the_path_to_the_folder, photo_name_and_extension), caption="Super Typhoon Maysak ")
+
 
 def main():
     your_login = os.getenv("LOGIN")
@@ -47,7 +34,8 @@ def main():
     parser.add_argument('path', help='your way to the folder')
     args = parser.parse_args()
     the_path_to_the_folder = args.path
-    instabot(your_login,your_password,the_path_to_the_folder)
+    instabot(your_login, your_password, the_path_to_the_folder)
+
 
 if __name__ == '__main__':
     main()
